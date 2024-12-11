34b46f0fb6ee50e7894c386ac2a7f4caf0b855c0
AryaRmdhn1210 <muhammadarya121005@gmail.com>
2024-12-11 13:06:35 +0800

    u: penyesuaian isi pada halaman depan”

---
+++ menu.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+void dMenu() {
+    system("cls");
+    cout << "Aplikasi Sorting Bubble\n";
+    cout << "1. Masukkan Data\n";
+    cout << "2. Tampilkan Data\n";
+    cout << "3. Sorting Ascending\n";
+    cout << "4. Sorting Descending\n";
+    cout << "5. Exit\n";
+    cout << "Masukkan angka : ";
+}
+
+void mMasukkanData() {
+    system("cls");
+    cout << "Menu Masukkan Data\n";
+    // Tambahkan logika input data di sini
+    getch();
+}
+
+void mTampilkanData() {
+    system("cls");
+    cout << "Menu Tampilkan Data\n";
+    // Tambahkan logika untuk menampilkan data di sini
+    getch();
+}
+
+void mSortingAsc() {
+    system("cls");
+    cout << "Menu Sorting Ascending\n";
+    // Tambahkan logika sorting ascending di sini
+    getch();
+}
+
+void mSortingDesc() {
+    system("cls");
+    cout << "Menu Sorting Descending\n";
+    // Tambahkan logika sorting descending di sini
+    getch();
+}
+
+int main() {
+    char pl;
+    do {
+        dMenu();
+        pl = getch();
+        switch (pl) {
+            case '1':
+                mMasukkanData();
+                break;
+            case '2':
+                mTampilkanData();
+                break;
+            case '3':
+                mSortingAsc();
+                break;
+            case '4':
+                mSortingDesc();
+                break;
+            case '5':
+                system("cls");
+                cout << "Keluar dari program.\n";
+                getch();
+                break;
+            default:
+                system("cls");
+                cout << "Pilihan Tidak Tersedia\n";
+                getch();
+                break;
+        }
+    } while (pl != '5');
+    return 0;
+}
